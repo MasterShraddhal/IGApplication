68e8dd4ea022855f32244b2a539ade31512ab018
shubham chavhan <spchavhan32@gmail.com>
2021-02-02 11:18:39 +0530

    modified views and serializer

--- instagapp/serializers.py
+++ instagapp/serializers.py
@@ -1,51 +1,56 @@
 from .models import *
-from rest_framework.serializers import ModelSerializer,ValidationError
+from rest_framework.serializers import ModelSerializer, ValidationError
+
 
 class UserSerializer(ModelSerializer):
     class Meta:
-        model=User
-        fields=['email','first_name','last_name','username','password','age','profile']
-        read_only_fields = ['id','status'] 
-        write_only_fields = ['password'] 
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name',
+                  'username', 'password', 'age', 'profile', 'status']
+        read_only_fields = ['id', 'status']
+        # write_only_fields = ['password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
-    def create(self,validated_data):
-        user_obj=User.objects.create_user(**validated_data)
+    def create(self, validated_data):
+        user_obj = User.objects.create_user(**validated_data)
         return user_obj
 
 
 class UpdateUserSerializer(ModelSerializer):
     class Meta:
-        model=User
-        fields=['first_name','last_name','username','profile','status']
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'profile', 'status']
 
 
 class ListFollowingSerializers(ModelSerializer):
-    following=UserSerializer()
-    class Meta:
-        model=FollowerInfo
-        fields=['id','following_id','following']
+    following = UserSerializer(read_only=True)
 
+    class Meta:
+        model = FollowerInfo
+        fields = ['id', 'following']
 
 
 class FollowerSerializer(ModelSerializer):
     class Meta:
-        model=FollowerInfo
-        fields=['following','status','id']
-        read_only_fields = ['status','id'] 
+        model = FollowerInfo
+        fields = ['following', 'status', 'id']
+        read_only_fields = ['status', 'id']
 
-    def create(self,validated_data):
+    def create(self, validated_data):
         print(validated_data)
-        Following=validated_data['following']
-        user=validated_data['user']
-        if Following.id==user.id:
+        Following = validated_data['following']
+        user = validated_data['user']
+        if Following.id == user.id:
             raise ValidationError("Cannot follow yourself")
         else:
-            if Following.status=="Public":
-                fobj=FollowerInfo(follower=user,following=Following,status="Accepted")
+            if Following.status == "Public":
+                fobj = FollowerInfo(
+                    follower=user, following=Following, status="Accepted")
                 fobj.save()
             else:
-                fobj=FollowerInfo(follower=user,following=Following,status="Pending")
+                fobj = FollowerInfo(
+                    follower=user, following=Following, status="Pending")
                 fobj.save()
         return fobj
-
-
--- instagapp/views.py
+++ instagapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .serializers import UserSerializer,UpdateUserSerializer,FollowerSerializer,ListFollowingSerializers
-from .models import User,FollowerInfo
-from rest_framework.generics import GenericAPIView,CreateAPIView,ListAPIView,RetrieveAPIView,DestroyAPIView,UpdateAPIView
-from rest_framework.permissions import AllowAny,BasePermission,IsAuthenticated
+from .serializers import UserSerializer, UpdateUserSerializer, FollowerSerializer, ListFollowingSerializers
+from .models import User, FollowerInfo
+from rest_framework.generics import GenericAPIView, CreateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
+from rest_framework.permissions import AllowAny, BasePermission, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from .permission import CustomUserPermission
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -11,42 +11,45 @@ from rest_framework.settings import api_settings
 from rest_framework.response import Response
 # Create your views here.
 
-class ListFollowingUsers(ListAPIView,GenericAPIView):
-    serializer_class=ListFollowingSerializers
-    authentication_classes=[TokenAuthentication]
-    permission_classes=[IsAuthenticated]
+
+class ListFollowingUsers(ListAPIView, GenericAPIView):
+    serializer_class = ListFollowingSerializers
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get_queryset(self):
-        user=self.request.user
-        fobjs=FollowerInfo.objects.filter(follower_id=user)
-        print(fobjs[0])
+        user = self.request.user
+        fobjs = FollowerInfo.objects.filter(follower_id=user)
         return fobjs
-        
 
-class FollowUsers(CreateAPIView,GenericAPIView):
-    queryset=FollowerInfo.objects.all()
-    serializer_class=FollowerSerializer
-    authentication_classes=[TokenAuthentication]
-    permission_classes=[IsAuthenticated]
 
-    def perform_create(self,serializer):
+class FollowUsers(CreateAPIView, GenericAPIView):
+    queryset = FollowerInfo.objects.all()
+    serializer_class = FollowerSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-class Unfollowuser(DestroyAPIView,RetrieveAPIView,GenericAPIView):
-    queryset=FollowerInfo.objects.all()
-    serializer_class=FollowerSerializer
-    authentication_classes=[TokenAuthentication]
-    permission_classes=[IsAuthenticated]
 
-    def perform_destroy(self,instance):
-        data=instance.__dict__
-        fid=data['follower_id']
-        if self.request.user.id==fid:
+class Unfollowuser(DestroyAPIView, RetrieveAPIView, GenericAPIView):
+    queryset = FollowerInfo.objects.all()
+    serializer_class = FollowerSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def perform_destroy(self, instance):
+        data = instance.__dict__
+        fid = data['follower_id']
+        if self.request.user.id == fid:
             instance.delete()
         else:
             raise Exception("User not authorized to perform this action")
 
+
 class CustomAuthToken(ObtainAuthToken):
-    def post(self,request,*args,**kwargs):
+    def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
                                            context={'request': request})
         serializer.is_valid(raise_exception=True)
@@ -60,24 +63,24 @@ class CustomAuthToken(ObtainAuthToken):
             'email': user.email
         })
 
-    renderer_classes=api_settings.DEFAULT_RENDERER_CLASSES
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
-class RegUser(CreateAPIView,ListAPIView,GenericAPIView):
-    queryset=User.objects.all()
-    serializer_class=UserSerializer
-    authentication_classes=[TokenAuthentication]
-    permission_classes=[CustomUserPermission,]
 
-class RetrieveUser(DestroyAPIView,RetrieveAPIView,UpdateAPIView,GenericAPIView):
-    queryset=User.objects.all()
-    #serializer_class=UserSerializer
-    authentication_classes=[TokenAuthentication]
-    permission_classes=[CustomUserPermission]
+class RegUser(CreateAPIView, ListAPIView, GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [CustomUserPermission, ]
+
+
+class RetrieveUser(DestroyAPIView, RetrieveAPIView, UpdateAPIView, GenericAPIView):
+    queryset = User.objects.all()
+    # serializer_class=UserSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [CustomUserPermission]
 
     def get_serializer_class(self):
-        if self.request.method=="PUT" or self.request.method=="PATCH":
+        if self.request.method == "PUT" or self.request.method == "PATCH":
             return UpdateUserSerializer
         else:
             return UserSerializer
-
-
